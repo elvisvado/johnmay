f28df8c4ea01991629e07b4edc25dcd11e046d84
xangmuve <cesarabel@johnmay.com.ni>
2015-12-30 08:20:16 -0600

    fix error in calcular --  import totalizar from api

--- base/models.py
+++ base/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from .base import *
+from django.db.models import Sum
+from .api import totalizar
 
 
 class Entidad(base_entidad):
